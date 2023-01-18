1b0ba8b9bbe26696feaaced3fc7e31e079f1406b
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2023-01-18 18:14:01 -0300

    exercicio ola spring

---
+++ src/main/java/com/springinit/init_spring/InitSpringApplication.java
@@ -0,0 +1,13 @@
+package com.springinit.init_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitSpringApplication.class, args);
+	}
+
+}
